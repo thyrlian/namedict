9087ecf32796d968e1eb56ac4d2afef4a72f655b
Jing Li <thyrlian@gmail.com>
2011-07-25 08:57:56 +0200

    Made a one-step post action "sweep" for all activate, set to private instance method only

--- lib/namedict/blacklist.rb
+++ lib/namedict/blacklist.rb
@@ -1,11 +1,14 @@
 # DSL implementation of Blacklist
 class Blacklist
+  attr_accessor :dict, :list
+
   def initialize(dict, &block)
     @dict = dict
+    @list = []
     instance_eval(&block)
+    @list.uniq!
+    sweep unless @list.size == 0
   end
-
-  private_class_method :new
 
   class << self
     def apply_to(dict, &block)
@@ -17,7 +20,17 @@ class Blacklist
     list_excluded = File.open(File.join(path_prj, 'data', 'Blacklist', file_list), "r:utf-8").inject([]) do |chars, line|
       chars << line.chomp
     end
-    list_excluded.uniq!
-    @dict.affect_by(list_excluded)
+    @list.concat(list_excluded)
+    # @dict.affect_by(list_excluded)
   end
+  
+  def sweep
+    # for each in @list, kick out from @dict
+    # create hash to speed up processing
+  end
+  
+  private_class_method :new
+  private :sweep
+  
+  return @dict
 end
