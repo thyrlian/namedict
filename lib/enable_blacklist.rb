2cb5d6502da7074b53b2bcbd03e29cfc4ddca52e
Jing Li <thyrlian@gmail.com>
2011-07-26 08:44:36 +0200

    Try and debug DSL implementation in enable_blacklist.rb

--- lib/enable_blacklist.rb
+++ lib/enable_blacklist.rb
@@ -11,7 +11,11 @@ path_prj = File.expand_path( File.join( File.dirname(__FILE__), ".." ) )
 file_chinese_character_list = File.join( path_prj, "data", "Modern Chinese Character Frequency List" )
 file_ps = File.join( path_prj, "data", "dict.dat" )
 dict = Dict.new(file_chinese_character_list, file_ps)
+puts dict.chars.size
+puts dict.chars[0].inspect
 
+
+=begin
 Blacklist.apply_to dict do
 
   activate "feminine"
@@ -20,3 +24,4 @@ Blacklist.apply_to dict do
   activate "other"
 
 end
+=end
