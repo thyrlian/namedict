cebc0434480b9e86796efd8b90a4490ad39978be
Jing Li <thyrlian@gmail.com>
2011-07-29 08:18:21 +0200

    Added a DSL based class of pronounceability

--- lib/namedict/pronounceability.rb
+++ lib/namedict/pronounceability.rb
@@ -1,3 +1,39 @@
+# DSL implementation of Pronounceability
+# Destroy method of new!  Passed parameter dict will be changed by reference
 class Pronounceability
-
+  attr_accessor :dict, :pronunciation
+  
+  def initialize(dict, &block)
+    @dict = dict
+    @pronunciation = {}
+    @path_prj = File.expand_path( File.join( File.dirname(__FILE__), "..", ".." ) )
+    instance_eval(&block)
+    validate unless @pronunciation.size == 0
+    dict.chars.replace(@dict)
+  end
+  
+  class << self
+    def affect(dict, &block)
+      new(dict, &block)
+    end
+  end
+  
+  def follow(file_lang)
+    pronounceable = File.open(File.join(@path_prj, 'data', 'Pronounceability', file_lang), "r:utf-8").inject({}) do |hsh, line|
+      hsh[line] = true if line.rm_comment!
+      hsh
+    end
+    @pronunciation.merge!(pronounceable)
+  end
+  
+  def validate
+    @dict.chars.each do |char|
+      char.py.each do |x|
+        break(true) if @pronunciation[x.gsub(/\d+/, "")]
+      end
+    end
+  end
+  
+  private_class_method :new
+  private :validate
 end
