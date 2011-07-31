df00823353dbf800af4cff2e7ccde9e2ab539f6b
Jing Li <thyrlian@gmail.com>
2011-07-31 10:28:48 +0200

    Removed several unnecessary require

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,8 +2,4 @@ $LOAD_PATH << "."
 $LOAD_PATH.unshift(File.join( File.dirname(__FILE__), "..", "lib" ))
 
 require "test/unit"
-require "rubygems"
-require "bigdecimal"
-require "pstore"
-require "json"
 require "namedict"
