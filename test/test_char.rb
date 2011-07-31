1bc003d43035fc5e3136ce230fe5572375f6da8e
Jing Li <thyrlian@gmail.com>
2011-07-31 11:05:14 +0200

    Added unit test of Char class

--- test/test_char.rb
+++ test/test_char.rb
@@ -3,4 +3,26 @@ $: << "."
 require "test_helper"
 
 class TestChar < Test::Unit::TestCase
+  def setup
+    path_prj = File.expand_path( File.join( File.dirname(__FILE__), ".." ) )
+    file_chinese_character_list = File.join( path_prj, "data", "Modern Chinese Character Frequency List" )
+    file_ps = File.join( path_prj, "data", "dict.dat" )
+    @char = Char.new(ch: "的", pc: BigDecimal.new("4.09432531783"), py: ["de", "di2", "di4"], tr: "(possessive particle)/of, really and truly, aim/clear")
+  end
+  
+  def test_ch
+    assert_equal("的", @char.ch)
+  end
+  
+  def test_pc
+    assert_equal(BigDecimal.new("4.09432531783"), @char.pc)
+  end
+  
+  def test_py
+    assert_equal(["de", "di2", "di4"], @char.py)
+  end
+  
+  def test_tr
+    assert_equal("(possessive particle)/of, really and truly, aim/clear", @char.tr)
+  end
 end
