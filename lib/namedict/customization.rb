e97848841b05c6f870c403492c3aa8cacda2b119
Jing Li <thyrlian@gmail.com>
2011-08-27 15:48:48 +0200

    Added super class Customization which has implemented most methods and a few abstract methods

--- lib/namedict/customization.rb
+++ lib/namedict/customization.rb
@@ -1,15 +1,44 @@
+# DSL implementation, should be inherited by subclass
+# Destroy method of new!  Passed parameter dict will be changed by reference
 class Customization
-  attr_accessor :dict, :list
+  attr_accessor :chars, :list
 
   def initialize(dict, &block)
-    @dict = dict
-    @list = []
+    @chars = dict.chars
+    @list = {}
     @path_prj = File.expand_path( File.join( File.dirname(__FILE__), "..", ".." ) )
+    set_dir_name
     instance_eval(&block)
-    @list.uniq!
-    sweep unless @list.size == 0
-    dict.chars.replace(@dict)
+    unless @list.size == 0
+      validate
+      dict.chars.replace(@chars)
+    end
   end
-
+  
+  class << self
+    def apply_to(dict, &block)
+      new(dict, &block)
+    end
+  end
+  
+  # Needs to be overridden by subclass
+  def set_dir_name
+    @dir_name = ""
+  end
+  
+  # Only has implemented getting list_loaded, needs post action for setting @list
+  def activate(file)
+    @list_loaded = File.open(File.join(@path_prj, 'data', @dir_name, file), "r:utf-8").inject({}) do |hsh, line|
+      hsh[line] = true if line.rm_comment!
+      hsh
+    end
+  end
+  
+  # Abstract method, needs to be implemented by subclass
+  def validate
+    raise NotImplementedError, "Abstract method is called, needs to be implemented."
+  end
+  
   private_class_method :new
+  private :validate
 end
