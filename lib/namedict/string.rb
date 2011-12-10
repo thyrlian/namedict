6ae20f9cdec702abec0788bad78c042378ad8761
Jing Li <thyrlian@gmail.com>
2011-12-11 00:06:55 +0100

    Added is_number_of_the_beast? method

--- lib/namedict/string.rb
+++ lib/namedict/string.rb
@@ -4,4 +4,14 @@ class String
     str = self.chomp.gsub(/#.*/, "").strip
     str == "" ? nil : self.replace(str)
   end
+  
+  # Only apply for Pinyin
+  # http://en.wikipedia.org/wiki/Number_of_the_Beast
+  # http://en.wikipedia.org/wiki/666_(number)
+  def is_number_of_the_beast?
+    number_of_name = self.downcase.split('').inject(0) do |num, ch|
+      num += ch.ord + 3
+    end
+    number_of_name == 666 ? true : false
+  end
 end
